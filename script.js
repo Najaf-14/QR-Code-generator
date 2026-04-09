c8ddefe40386e0aed333ee07e7445f15a420fdba
Najaf-14 <najafimran314@gmail.com>
2026-04-09 22:45:15 +0500

    Complete

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ let QRtext = document.getElementById("QR-text");
 let QRbox = document.getElementById("QR-box");
 let QRimg = document.getElementById("QR-img");
 
+let downloadBtn = document.getElementById("download-btn");
+
 function generateQR() {
   if (QRtext.value.length > 0) {
-    QRimg.src ="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRtext.value;
+    QRimg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      QRtext.value;
     QRbox.classList.add("show-img");
+    downloadBtn.style.display = "block";
   } else {
     QRtext.classList.add("error");
     setTimeout(() => {
-      QRtext.classList.add("error");
+      QRtext.classList.remove("error");
     }, 1000);
   }
+}
+
+function downloadQR() {
+  const link = document.createElement("a");
+  link.href = QRimg.src;
+  link.download = "qr-code.png";
+  link.click();
 }
